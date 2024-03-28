29511bcdf7611f133148fac4f74e353354850efa
Valerie St-Pierre <vst-pier@c2r2p7.42quebec.com>
2024-03-28 16:27:26 -0400

    ex00

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -4,14 +4,14 @@
 #include <iostream>
 
 template<typename A>
-void iter(A *array, int size, void(f)(const &A))
+void iter(A *array, int size, void(f)(const A&))
 {
 	for (int i = 0; i < size; i++)
 		f(array[i]);
 }
 
 template<typename A>
-void is_even(const &A a)
+void is_even(const A& a)
 {
 	if(a % 2 == 0)
 		std::cout << "Even" << std::endl;
@@ -20,9 +20,16 @@ void is_even(const &A a)
 }
 
 template<typename A>
-void print(const &A a)
+void print(const A& a)
 {
 	std::cout << a << std::endl;
+}
+
+template<typename A>
+void square(const A& a)
+{
+	A b = a * a;
+	std::cout << b << std::endl;;
 }
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,17 +5,11 @@
 int main()
 {
 	int intarray[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	char chararray[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	char chararray[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
 	iter(intarray, 10, is_even);
-	for(int i = 0 ; i < 10; i ++)
-		std::cout << intarray[i] << std::endl;
 	iter(intarray, 10, print);
-	for(int i = 0 ; i < 10; i ++)
-		std::cout << intarray[i] << std::endl;
+	iter(intarray, 10, square);
 	iter(chararray, 10, is_even);
-	for(int i = 0 ; i < 10; i ++)
-		std::cout << chararray[i] << std::endl;
 	iter(chararray, 10, print);
-	for(int i = 0 ; i < 10; i ++)
-		std::cout << chararray[i] << std::endl;
+	iter(chararray, 10, square);
 }
